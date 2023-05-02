226e91cc160181f489d937ed71dce8b47b2fa7d5
Artem <memskov@gmail.com>
2023-05-02 12:04:12 +0200

    comments added

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 CREDENTIALS_PATH = 'credentials.json'
-SHEET_NAME = 'LaOne'
-ASSORTMENT_PATH = 'json/assortment.json'
-STOCKS_PATH = 'json/stocks.json'
+# Путь к json файлу, который можно получить в https://console.cloud.google.com/apis/credentials
+
+SHEET_NAME = 'LaOne'  # Название Документа в Google Disk
+
+ASSORTMENT_PATH = 'json/assortment.json'  # Путь до assortment.json
+STOCKS_PATH = 'json/stocks.json'  # Путь до stocks.json
